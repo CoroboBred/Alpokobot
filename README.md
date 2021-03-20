64f7eb0faeff1773c419c736c0de7f604f10c7c6
Corobo <kingcorobobred@gmail.com>
2021-03-20 13:40:19 -0400

    Add config and commands
    Add config template
    Add "!cat", "!emote", and "!command"
    commands

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Alpokobot
 My personal twitch bot
+
+
+# Config
+To use the bot yourself just modify the `config.json` file and run the bot with `node bot.js`.
+
+# Dependencies
+You will need to install `npm` to use the bot. The current dependency list is:
+* tmi.js
+
+These dependencies can be installed with `npm install`.
---
+++ bot.js
@@ -0,0 +1,38 @@
+const tmi = require('tmi.js');
+const config = require('./config.json');
+
+// Create a client with our options
+const client = new tmi.client(config);
+
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
+  if (commandName == "ab"){console.log(commandName);}
+
+  // If the command is known, let's execute it
+  if (commandName === '!command' || commandName === "!commands") {
+    client.say(target, `Supported commands: !cat !emote`);
+    console.log(`* Executed ${commandName} command`);
+  } else if (commandName === "!cat" || commandName === "!cats") {
+    client.say(target, "Learn about my custom made bongo cat crew at: github.com/CoroboBred/Bongo-Cat-Crew")
+  } else if (commandName == "!emote" || commandName === "!emotes"){
+      client.say( target, "catJAM blobDance bongoTap pepeJAM HYPERS ppJedi AYAYA")
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
